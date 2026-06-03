38857f5655cabc0b2b555763e82cd03977112bbd
4large <fourlarge@outlook.com>
2026-06-02 19:32:05 -0600

    NOT DIRECTED AT JIMMY IN ANY WAY

--- title.js
+++ title.js
@@ -1,10 +1,22 @@
 const title = new Image();
 title.src = 'assets/title.png';
 
+const kirkington = document.createElement('video');
+let videoLoaded = false
+kirkington.oncanplay = () => { videoLoaded = true; };
+kirkington.src = 'assets/erika-kirk-kirk.mp4';
+kirkington.autoplay = true;
+kirkington.loop = true;
+
 export function updateTitle(ctx, dt) {
-  
+
 }
 
 export function drawTitle(ctx, canvas) {
-  ctx.drawImage(title, 0, 0, canvas.width, title.height);
+    if (videoLoaded) {
+        for (let x = 0; x < 1200; x += 250) {
+            ctx.drawImage(kirkington, x, 200);
+        }
+    }
+    ctx.drawImage(title, 0, 0, canvas.width, title.height);
 }
