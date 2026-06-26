8f2e99711877aa185ef35587d515283293a378cd
4large <fourlarge@outlook.com>
2026-06-26 14:18:53 -0600

    ads functional

--- ad.js
+++ ad.js
@@ -2,15 +2,24 @@
 import Swal from 'https://cdn.jsdelivr.net/npm/sweetalert2@11/+esm';
 
 export function playad() {
-    //pick random ad from ad folder
-    //hardcode my fucking ass, this is gonna need updating with any ads that get added
-    const ads = [
-        'assets/ads/furry.png',
-        'assets/ads/kirkingmyschmeet.webp',
-        'assets/ads/onlyguys.webp',
-        'assets/ads/residente.png',
-        'assets/ads/sexysolitaire.webp'
-    ];
+  //pick random ad from ad folder
+  //hardcode my fucking ass, this is gonna need updating with any ads that get added
+  const ads = [
+    'assets/ads/furry.png',
+    'assets/ads/kirkingmyschmeet.webp',
+    'assets/ads/onlyguys.webp',
+    'assets/ads/residente.png',
+    'assets/ads/sexysolitaire.webp'
+  ];
 
-    //TODO: Randomly select ad and display it using Swal.
+  //Pick random ad then play it in Swal
+  let ad = Math.floor(Math.random() * (ads.length - 1));
+
+  Swal.fire({
+    imageUrl: ads[ad],
+    timer: 5000,
+    text: 'Brought to you by Benjamin Netanyahu INC.',
+    allowOutsideClick: false,
+    showConfirmButton: false
+  });
 }
